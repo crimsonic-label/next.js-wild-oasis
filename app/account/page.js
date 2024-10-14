97e3206a634d457caab5efe4827c6cc2ff9bab70
Andrzej D <adrozdow@o2.pl>
2024-10-14 21:16:44 +0200

    layout

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,9 @@
+import Navigation from "../components/Navigation";
+
 export default function Page() {
-  return <h1>Your account</h1>;
+  return (
+    <div>
+      <h1>Your account</h1>;
+    </div>
+  );
 }
