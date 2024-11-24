3db39b5920b83c84dfdac95d425a6daac96b006c
Andrzej D <adrozdow@o2.pl>
2024-11-24 21:57:28 +0100

    sign out action connected

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,8 +1,12 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 // server action because we cannot run signIn from client side button
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
