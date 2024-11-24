d9b617edf66412d5a46881157df15c467cbc11f5
Andrzej D <adrozdow@o2.pl>
2024-11-24 15:30:26 +0100

    authorisation for path - session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,6 +8,12 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  // used for middleware.js
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
