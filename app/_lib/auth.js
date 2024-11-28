a8de6f9bd7b721a049cb8600702aa2ba12d38ecf
Andrzej D <adrozdow@o2.pl>
2024-11-28 20:51:03 +0100

    auth functions

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,25 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    // after used send login but before is logged to appliaction
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        // if there is not guest for logged user
+        if (!existingGuest)
+          await createGuest({ email: user.email, full_name: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    // called after signIn callback
+    async session({ session, user }) {
+      // add guest id to the session
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.guestId;
+      return session;
     },
   },
   pages: {
