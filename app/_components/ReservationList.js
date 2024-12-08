7f5f553c41e40b981afa47deb66fd5b8d5c517da
Andrzej D <adrozdow@o2.pl>
2024-12-08 15:45:00 +0100

    useOptimistic for delete reservation

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,38 @@
+"use client";
+import ReservationCard from "./ReservationCard";
+import { useOptimistic } from "react";
+import { deleteReservation } from "@/app/_lib/actions";
+
+function ReservationList({ bookings }) {
+  // optimisticDelete - setter function
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings, // actual state
+    // function for optimistic state, gets arguments: current state and all arguments for functions
+    // calculate what should be optimistic results
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  // function jus to delete reservation
+  async function handleDelete(bookingId) {
+    // call optimistic
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        // bookings replaced with optimistic bookings
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
